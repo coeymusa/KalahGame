f0cd38cb939e7f3cb782bffbec633be95f16271a
coeymusa <coreymusa@outlook.com>
2018-11-12 17:25:16 +0000

    Initial commit

---
+++ src/main/java/com/kalah/game/KalahGameApplication.java
@@ -0,0 +1,12 @@
+package com.kalah.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KalahGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KalahGameApplication.class, args);
+	}
+}
