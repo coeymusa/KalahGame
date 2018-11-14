4300557b5f2ad44c3ce3af7d13b2a235677b571a
coeymusa <coreymusa@outlook.com>
2018-11-14 00:52:05 +0000

    mongodb

--- src/main/java/com/kalah/game/repository/KalahRepository.java
+++ src/main/java/com/kalah/game/repository/KalahRepository.java
@@ -1,10 +1,9 @@
 package com.kalah.game.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import com.kalah.game.service.Game;
 
-@Repository
-public interface KalahRepository extends CrudRepository<Game, String> {
+public interface KalahRepository extends MongoRepository<Game, String> {
 
 }
